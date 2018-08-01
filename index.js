ce68a1436388702f4c710cf1e3be51bc7ecdaa56
Matthew Starkey <mattstarkey@me.com>
2018-08-01 19:04:42 +0200

    Demo Stream URL

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Stream = require('./node-rtsp-stream');
 const streamUrl = process.env.FOSCAM_STREAM_URL;
+const devUrl = 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov';
 
 stream = new Stream({
   name: 'foscam_stream',
-  streamUrl: streamUrl,
+  streamUrl: devUrl,
   wsPort: 9999,
   width: 1280/2,
   height: 720/2,
