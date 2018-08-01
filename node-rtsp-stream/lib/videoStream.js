b4685fe7b326df003e98fedf8ac93f2e251e3554
Matthew Starkey <mattstarkey@me.com>
2018-08-01 22:30:52 +0200

    test

--- node-rtsp-stream/lib/videoStream.js
+++ node-rtsp-stream/lib/videoStream.js
@@ -87,10 +87,10 @@
     this.wsServer.broadcast = function(data, opts) {
       var i, _results;
       _results = [];
-      console.log(this.clients);
       for (i in this.clients) {
         if (this.clients[i].readyState === 1) {
           _results.push(this.clients[i].send(data, opts));
+          console.log(i);
         } else {
           _results.push(console.log("Error: Client (" + i + ") not connected."));
         }
