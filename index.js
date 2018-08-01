6d498badbb19af6811abfc7b39bccfb4a02e1c24
Matthew Starkey <mattstarkey@me.com>
2018-08-01 21:19:52 +0200

    Serve test client

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ stream = new Stream({
   name: 'foscam_stream',
   streamUrl: devUrl,
   wsPort: 9999,
-  width: 1280/2,
-  height: 720/2,
+  width: 1280 / 2,
+  height: 720 / 2,
   fps: '24',
   kbs: '256k'
 });
+
+var url = require("url");
+var http = require("http");
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+  // var path = url.parse(request.url).pathname;
+
+  fs.readFile(__dirname + '/test_client.html', function (error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write(error);
+      response.end();
+    } else {
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      response.write(data);
+      response.end();
+    }
+  });
+
+});
+server.listen(8082);
