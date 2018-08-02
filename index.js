68ea56cb98fb05060e5bee4e03a79eebb0666bc2
Matthew Starkey <mattstarkey@me.com>
2018-08-02 13:06:35 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const Stream = require('./node-rtsp-stream');
 const streamUrl = process.env.FOSCAM_STREAM_URL;
 const devUrl = 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov';
+var ws = require('ws');
 
-stream = new Stream({
-  name: 'foscam_stream',
-  streamUrl: devUrl,
-  wsPort: 9797,
-  width: 1280 / 2,
-  height: 720 / 2,
-  fps: '24',
-  kbs: '256k'
+// stream = new Stream({
+//   name: 'foscam_stream',
+//   streamUrl: devUrl,
+//   wsPort: 9797,
+//   width: 1280 / 2,
+//   height: 720 / 2,
+//   fps: '24',
+//   kbs: '256k'
+// });
+
+
+var wsServer = new ws.Server({
+  port: 9797
+});
+wsServer.on("connection", function(socket) {
+  socket.send('test');
+  console.log('Connection');
 });
 
 
