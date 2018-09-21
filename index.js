75051ef883c7f82cdb53f05d2af72040d8b4f8aa
Matthew Starkey <mattstarkey@me.com>
2018-09-21 14:31:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ stream = new Stream({
   name: 'foscam_stream',
   streamUrl: streamUrl,
   wsPort: 9797,
-  width: 1920,
-  height: 1080,
+  width: 1280,
+  height: 720,
   fps: '24',
   kbs: '512k'
 });
